a870b4541463eb668e89f59aab2c54a62315bae6
GandalfZ <93980496+GandalfZ@users.noreply.github.com>
2021-12-28 14:26:37 +0100

    feat: Datenaufbereitung (#1)

---
+++ src/data.js
@@ -0,0 +1,54 @@
+import {nanoid} from "nanoid";
+
+export const teamMembers = [
+    {
+        id: nanoid(),
+        firstName: "Bejan",
+        lastName: "Afshar"
+    },
+    {
+        id: nanoid(),
+        firstName: "Sabrina",
+        lastName: "Müller"
+    },
+    {
+        id: nanoid(),
+        firstName: "Helena",
+        lastName: "Heil"
+    },
+    {
+        id: nanoid(),
+        firstName: "Farah",
+        lastName: "Mansour"
+    },
+    {
+        id: nanoid(),
+        firstName: "Sharine",
+        lastName: "Ankenbrandt"
+    },
+    {
+        id: nanoid(),
+        firstName: "Oliver",
+        lastName: "Grammel"
+    },
+    {
+        id: nanoid(),
+        firstName: "Felix",
+        lastName: "Canditt"
+    },
+    {
+        id: nanoid(),
+        firstName: "Miriam",
+        lastName: "Hähnle"
+    },
+    {
+        id: nanoid(),
+        firstName: "Jens",
+        lastName: "Dreske"
+    },
+    {
+        id: nanoid(),
+        firstName: "Sacid",
+        lastName: "Celik"
+    },
+]
