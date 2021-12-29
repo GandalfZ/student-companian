c1caf30658a3476676ea07ee2c5ecdc224741c5d
Bejan A <da_reggaeman@gmx.de>
2021-12-29 18:54:47 +0100

    added background svg to assets and built header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,32 @@
+import styled from "styled-components";
+import { ReactComponent as BackgroundImage } from "../assets/background_shape_1.svg";
+
 const Header = () => {
-    return <h1>Energy</h1>
+    return (
+        <>
+            <HeaderWrapper>
+                <BackgroundImage />
+            </HeaderWrapper>
+            <TitleWrapper>
+                <Title>Title</Title>
+            </TitleWrapper>
+        </>
+    )
 }
 
 export default Header;
+
+const HeaderWrapper = styled.div`
+height: 3.688rem;
+top: 0;
+position: absolute;
+width: 375px;
+`
+const TitleWrapper = styled.div`
+position: relative;
+text-align: center;
+width: 375px;
+`
+const Title = styled.h1`
+margin: 0;
+`
