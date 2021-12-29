b5d1f60c122da2a2690162dbee3b3fb856ccc971
Bejan A <da_reggaeman@gmx.de>
2021-12-29 10:26:03 +0100

    deleted links in global styles

--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -2,11 +2,6 @@ import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet">
-
-
 *,
 *::before,
 *::after {
